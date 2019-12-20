7d776863d87f64c702a064d69e77254acca3a3c8
max.jahn@gmail.com <maximillian.jahn@oracle.com>
2019-12-20 09:43:51 +0100

    updated to modify path in sqlnet.ora

--- scripts/get_wallet.sh
+++ scripts/get_wallet.sh
@@ -4,5 +4,13 @@ oci db autonomous-database generate-wallet --autonomous-database-id "${ATP_OCID}
 
 cd ../resources
 unzip -o wallet.zip -d ./wallet/
+
+cd wallet
+export WALLET_DIR=`pwd`
+
+sed -i "s|DIRECTORY=\"[^\"]*|DIRECTORY=\"${WALLET_DIR}|" sqlnet.ora 
+
+export TNS_ADMIN=${WALLET_DIR}
+
 cd ~
 
